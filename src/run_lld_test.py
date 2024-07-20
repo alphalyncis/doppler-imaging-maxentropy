1a81fb7b8d172c8bbf52b8759ca3eceb05330f1a
Xueqing Chen <levinedeche@hotmail.com>
2024-07-20 14:37:51 +0100

    lld test

---
+++ src/run_lld_test.py
@@ -0,0 +1,46 @@
+import paths
+import os
+import numpy as np
+from dime import DopplerImaging, load_data_from_pickle
+from config import load_config
+
+instru = "IGRINS"
+target = "W1049B"
+band = "H"
+savedir = f"{instru}_lldtest"
+
+params, goodchips, modelspec = load_config(instru, target, band)
+model_datafile = paths.data/f'fitted/{instru}_{target}_{band}_{modelspec}.pickle'
+#goodchips = [1,2,3,4]
+
+if not os.path.exists(paths.figures/savedir):
+    os.makedirs(paths.figures/savedir)
+
+##############################################################################
+####################      Run!      ##########################################
+##############################################################################
+
+# Load data from pickle fit
+wav_nm, template, observed, error = load_data_from_pickle(model_datafile, goodchips)
+
+dmap = DopplerImaging(wav_nm, goodchips, params_dict=params)
+dmap.load_data(observed, template, error)
+dmap.make_lsd_profile(modelspec, plot_lsd_profiles=True, plot_deviation_map=True)
+
+for lld in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]:
+    dmap.lld = lld
+    dmap.solve(create_obs_from_diff=True, solver='scipy')
+
+
+    dmap.plot_mollweide_map(vmin=85, vmax=110, savedir=paths.figures/f"{savedir}/lld_{lld}.png")
+
+
+
+
+#LSDlin_map = solve_LSD_starry_lin(intrinsic_profiles, obskerns_norm, kwargs_run, kwargs_fig, annotate=False, colorbar=False)
+#LSDopt_map = solve_LSD_starry_opt(intrinsic_profiles, obskerns_norm, kwargs_run, kwargs_fig, lr=lr_LSD, niter=5000, annotate=False, colorbar=False)
+#lin_map = solve_starry_lin(mean_spectrum, observed, wav_nm, wav0_nm, kwargs_run, kwargs_fig, annotate=False, colorbar=False)
+#opt_map = solve_starry_opt(mean_spectrum, observed, wav_nm, wav0_nm, kwargs_run, kwargs_fig, lr=lr, niter=5000, annotate=False, colorbar=False)
+
+print("Run success.")
+
